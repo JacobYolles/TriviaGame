0b1ec8e915af3c8d272c47e9273ce25dfd704679
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-08-08 09:21:42 -0600

    added new structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,15 +4,33 @@ $(document).ready(function() {
 
 
     // Setting variables first!
-var questionArray = [];
-var answerArray = [];
-var correctAnswers = [];
+var questionArray = ["What website do users send tweets on", "What does HTML stand for?", "What does CSS stand for?", "What is a boolean?", "var stands for?"];
+var answerArray = [ ["Facebook", "Twitter", "Steam", "Google",], ["Hyper Textual Marking Laundry", "Hyper Text Markup Language", "Hydrated Types Make Liquid", "Happy Typos Master Libraries"], ["Cascading Style Sheets", "Casablancas Super Soakers", "Controling Style Sheets", "Constable Selective Syntax",], ["A yes or no variable", "A true or false variable", "A string value", "A picnic in the park",], ["A variable", "A varsety team", "A variety of things", "A varied type of item",], ];
+var images = [];
+var correctAnswers = ["B. Twitter", "B. Hypter Text Markup Language", "A. Cascading Style Sheets", "B. A true or false variable", "A. A variable"];
 var questionCounter = 0;
+var selecterAnswer; 
+var counter = 30;
+var theClock;
+var correctTally = 0;
+var incorrectTally = 0;
+var unansweredTally = 0;
+
+
+// CREATING FUNCTIONS
 
 
 
 
-
-
+// CREATING A RESET FUNCTION: 
+function resetGame() {
+    questionCounter = 0;
+    correctTally = 0;
+    incorrectTally = 0;
+    unansweredTally = 0;
+    counter = 30;
+    generateHTML();
+    timerWrapper();
+}
 
 }); // End result for the document ready function.
